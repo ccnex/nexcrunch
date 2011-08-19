28dd25f894b71b1362f119fed7539eecb711e8df
cifu <ccifuentes@nexica.com>
2011-08-19 21:16:02 +0200

    el main xD

---
+++ ElasticCloud/ElasticCloud.php
@@ -0,0 +1,11 @@
+<?php
+//El main en si mismo :P
+include_once 'src/LogicController.php';
+
+//instanciamos un contraldor de todo el tinglado
+$controller = new LogicController(0);
+
+//aremos algo con el, ya veremos...
+
+?>
+
